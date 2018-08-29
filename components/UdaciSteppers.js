2e0a2ff4d7f18b1a07a98e3f05a5b88b5ce05979
krzysztofHadas <hada10@gazeta.pl>
2018-08-29 12:53:26 +0200

    make the buttons looks androidy on android

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
-import { FontAwesome, white, purple, gray } from '@expo/vector-icons'
+import { FontAwesome, white, purple, gray, Entypo } from '@expo/vector-icons'
 
 
 const UdaciSteppers = ({ max, unit, step, value, onIncrement, onDecrement }) => {
     console.log("wszystko", value, unit);
     return (
         <View style={[styles.row, {justifyContent: 'space-between'}]}>
-            <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity onPress={onDecrement} style={[styles.iOSBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
-                    <FontAwesome name='minus' size={30} color={purple} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onIncrement} style={[styles.iOSBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
-                    <FontAwesome name='plus' size={30} color={purple} />
-                </TouchableOpacity>
-            </View>
+            {
+                Platform.OS === 'ios' 
+                ?   <View style={{flexDirection: 'row'}}>
+                        <TouchableOpacity onPress={onDecrement} style={[styles.iOSBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+                            <Entypo name='minus' size={30} color={purple} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={onIncrement} style={[styles.iOSBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
+                            <Entypo name='plus' size={30} color={purple} />
+                        </TouchableOpacity>
+                    </View>
+                :   <View style={{flexDirection: 'row'}}>
+                        <TouchableOpacity onPress={onDecrement} style={[styles.androidBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+                            <FontAwesome name='minus' size={30} color={white} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={onIncrement} style={[styles.androidBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
+                            <FontAwesome name='plus' size={30} color={white} />
+                        </TouchableOpacity>
+                    </View>
+            }
+            
             <View style={styles.metricCounter}>
                 <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
                 <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
@@ -37,6 +49,12 @@ const styles = StyleSheet.create({
         padding: 5,
         paddingLeft: 25,
         paddingRight: 25,
+    },
+    androidBtn: {
+        margin: 5,
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 2,
     },
     metricCounter: {
         width: 85,
