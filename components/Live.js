6e1acb063250cec9b18cdd3661fee5d1f7edd0e2
krzysztofHadas <hada10@gazeta.pl>
2018-09-04 15:27:22 +0200

    styles created for live component

--- components/Live.js
+++ components/Live.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import { Text, View, ActivityIndicator } from 'react-native'
+import { Text, View, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native'
+import {Foundation } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
-    direction = '',
+    status: 'undetermined',
+    direction: '',
   }
 
   render() {
-    const { coords, status, null } = this.state
+    const { coords, status, direction } = this.state
     
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}} />
@@ -39,3 +41,27 @@ export default class Live extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  center: { 
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  }
+})
